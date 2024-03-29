a32e3180e9a290fed317900897f57f0b9db511bc
thenatural86 <philip.smalls86@gmail.com>
2024-03-29 17:41:57 -0300

    setup front end

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 require('express-async-errors')
 
+const path = require('path')
+
 // extra security packages
 const helmet = require('helmet')
 const xss = require('xss-clean')
@@ -18,13 +20,15 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
 app.use(helmet())
 app.use(xss())
 
-// app.get('/', (res, req) => {
-//   req.send('YOLO')
-// })
+app.get('*', (res, req) => {
+  req.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
