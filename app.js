d596770242bd3d785eebcbc5e2f94422725318a5
thenatural86 <philip.smalls86@gmail.com>
2024-03-30 15:30:24 -0300

    fix bug

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const path = require('path')
 // extra security packages
 const helmet = require('helmet')
 const xss = require('xss-clean')
-const rateLimiter = require('express-rate-limit')
 
 const express = require('express')
 const app = express()
@@ -20,18 +19,20 @@ const jobsRouter = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.set('trust proxy', 1)
+
 app.use(express.static(path.resolve(__dirname, './client/build')))
 app.use(express.json())
 app.use(helmet())
 app.use(xss())
 
-app.get('*', (res, req) => {
-  req.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
-})
-
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
