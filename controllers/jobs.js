3da2b2b0bbd608235c4e0bec917aa1d457bef270
thenatural86 <philip.smalls86@gmail.com>
2024-03-31 17:51:19 -0300

    search functionality

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -3,8 +3,58 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
-  res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
+  const { search, status, jobType, sort } = req.query
+
+  // protected route
+  const queryObject = { createdBy: req.user.userId }
+
+  if (search) {
+    queryObject.position = { $regex: search, $options: 'i' }
+  }
+
+  // add things based on condition
+  if (status && status !== 'all') {
+    queryObject.status = status
+  }
+
+  if (jobType && jobType !== 'all') {
+    queryObject.status = status
+  }
+
+  // NO AWAIT
+
+  let result = Job.find(queryObject)
+
+  // chain sort conditions
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt')
+  }
+
+  if (sort === 'oldest') {
+    result = result.sort('createdAt')
+  }
+
+  if (sort === 'a-z') {
+    result = result.sort('position')
+  }
+
+  // setup pagination
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
+  const jobs = await result
+
+  const totalJobs = await Job.countDocuments(queryObject)
+
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  // const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages })
 }
 const getJob = async (req, res) => {
   const {
